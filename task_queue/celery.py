3bbe26e2dd6368665e46809cbb29855735ad8a12
MarianoSaez <mm.saez@alumno.um.edu.ar>
2022-02-10 22:44:38 -0300

    Dockerizacion de: Servidor, Redis y Task Queue en Celery. Ahora es posible levantar la infraestructura con docker-compose

--- task_queue/celery.py
+++ task_queue/celery.py
@@ -2,13 +2,14 @@
 Instancia de Celery a ser importada por modulos que contengan
 funciones llamadas de forma remota.
 """
+import os
 from celery import Celery
 
 
 app = Celery(
     "task_queue",
-    broker="redis://localhost",
-    backend="redis://localhost",
+    broker=os.environ.get("CELERY_BROKER_URL"),
+    backend=os.environ.get("CELERY_BACKEND_URL"),
     include=[
         "task_queue.scrap_tasks",
     ]
